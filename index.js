064ac7c385c047ef648df512b5c7dd11cf0548a0
kelvin901 <wawerukelvin2019@gmail.com>
2023-03-19 17:41:28 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,66 @@
-// Write your solution in this file!
+let employee = {
+    name: "Kelvin",
+    streetAddress: "Moringa School"
+  };
+  console.log(employee.name); // "Kelvin"
+  console.log(employee["streetAddress"]); // "Moringa School"
+  
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+      ...employee,
+      [key]: value
+    };
+  }
+
+  let newemployee = {
+    name: "Kelvin",
+    streetAddress: "Moringa School"
+  };
+  
+  let newEmployee = updateEmployeeWithKeyAndValue(employee, "name", "Alice");
+  
+  console.log(employee); // { name: "Kelvin", streetAddress: "Moringa School" }
+  console.log(newEmployee); // { name: "Alice", streetAddress: "Moringa School" }
+
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  let updatedemployee = {
+    name: "Kelvin",
+    streetAddress: "Moringa School"
+  };
+  
+  destructivelyUpdateEmployeeWithKeyAndValue(employee, "name", "Alice");
+  
+  console.log(employee); // { name: "Alice", streetAddress: "Moringa School" }
+    
+  function deleteFromEmployeeByKey(employee, key) {
+    let newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+  }
+  let nemployee = {
+    name: "Kelvin",
+    streetAddress: "Moringa School"
+  };
+  
+  let Employee = deleteFromEmployeeByKey(employee, "name");
+  
+  console.log(employee); // { name: "Kelvin", streetAddress: "Moringa School" }
+  console.log(newEmployee); // { streetAddress: "Moringa School" }
+    
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
+
+  let destructedemployee = {
+    name: "Kelvin",
+    streetAddress: "Moringa School"
+  };
+  
+  destructivelyDeleteFromEmployeeByKey(employee, "name");
+  
+  console.log(employee); // { streetAddress: "Moringa School" }
+  
